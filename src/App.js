daff7fd98f3cfbe4f1576bda5200f84048d75c71
shreyanshandilya <94351863+shreyanshandilya@users.noreply.github.com>
2024-04-08 21:07:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,13 @@ import {
 } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import Home from "./pages/home";
-import Navbar from "./Navbar/navbar";
-import { BrowserRouter } from "react-router-dom";
-// import Timeline from "./Component/Timeline";
 
 function App() {
    let router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" elemenet={<Home />}>
         <Route path="about" element={<AboutPage />} />
-   
+
       </Route>
     )
   );
@@ -24,16 +21,6 @@ function App() {
   return (
     <>
         <RouterProvider router={router} />
-      <BrowserRouter>
-        {" "}
-        <div>
-          <Navbar />
-          <Hero />
-          {/* <Timeline /> */}
-        </div>
-      </BrowserRouter>
     </>
   );
 }
-
-export default App;
