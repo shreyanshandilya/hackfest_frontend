f30b09931f3b9ecfe276cb77ea0e8c7c40c12561
suyash suryavanshi <suyashsuryavanshi11111@gmail.com>
2024-04-24 03:14:27 +0530

    Update RegistrationButton.jsx

--- src/Component/RegistrationButton.jsx
+++ src/Component/RegistrationButton.jsx
@@ -3,7 +3,7 @@ import './button.css'
 function Component() {
     return (
       <div>
-        <a href="https://bit.ly/hackfest24" target="blank">
+        <a href="https://unstop.com/hackathons/hackfest-24-indian-institute-of-technology-indian-school-of-mines-957047" target="blank">
             <button className='btn-unstop'>
                 Register on UNSTOP
             </button>
